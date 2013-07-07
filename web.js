17bead2984dd53fd7eacaabfba1fbf10a698ca31
yansun05 <yansun05@gmail.com>
2013-07-07 16:40:08 -0700

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var indexBuffer = fs.readFileSync('index.html').toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexBuffer);
 });
 
 var port = process.env.PORT || 5000;
